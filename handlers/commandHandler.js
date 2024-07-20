f072053155a4117b23edf431f5606f500535d341
himon12 <100027969+himon12@users.noreply.github.com>
2024-07-20 15:44:00 +0545

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 module.exports = (client) => {
     client.commands = new Collection();
+    client.prefix = '+';
 
     const commandFiles = fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js'));
 
@@ -13,6 +14,25 @@ module.exports = (client) => {
             client.commands.set(command.data.name, command);
         }
     }
+
+    client.on('messageCreate', async (message) => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(client.prefix)) return;
+
+        const args = message.content.slice(client.prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(message, args);
+        } catch (error) {
+            console.error(error);
+            await message.reply('There was an error while executing this command!');
+        }
+    });
 
     client.on('interactionCreate', async (interaction) => {
         if (!interaction.isCommand()) return;
@@ -29,3 +49,4 @@ module.exports = (client) => {
         }
     });
 };
+            

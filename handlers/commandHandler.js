808d455e979d2c7e7df477a33fc592e89cc1fb86
himon12 <100027969+himon12@users.noreply.github.com>
2024-07-20 15:19:49 +0545

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,13 +1,31 @@
+const { Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { Collection } = require('discord.js');
 
 module.exports = (client) => {
     client.commands = new Collection();
+
     const commandFiles = fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js'));
 
     for (const file of commandFiles) {
         const command = require(path.join(__dirname, '../commands', file));
-        client.commands.set(command.data.name, command);
+        if (command.data) {
+            client.commands.set(command.data.name, command);
+        }
     }
+
+    client.on('interactionCreate', async (interaction) => {
+        if (!interaction.isCommand()) return;
+
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    });
 };
